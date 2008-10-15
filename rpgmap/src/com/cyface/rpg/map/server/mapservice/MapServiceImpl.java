9d2c1010eae69f2e63835f47015181c50f599bb8
cyface <cyface@1b8295e0-9937-11dd-bde8-492b52a95b86>
2008-10-15 10:41:34 +0000

    Added service and test.

--- rpgmap/src/com/cyface/rpg/map/server/mapservice/MapServiceImpl.java
+++ rpgmap/src/com/cyface/rpg/map/server/mapservice/MapServiceImpl.java
@@ -1,8 +1,32 @@
 package com.cyface.rpg.map.server.mapservice;
 
+import java.util.ArrayList;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.log4j.Logger;
+
+import com.cyface.rpg.map.server.Map;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 public class MapServiceImpl extends RemoteServiceServlet {
 	private static final long serialVersionUID = 1L;
+	Logger logger = Logger.getLogger(com.cyface.rpg.map.server.mapservice.MapServiceImpl.class);
 
+	
+	ArrayList<Map> getAllMaps() {
+		ArrayList<Map> resultList = new ArrayList<Map>();
+		try {
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("rpgmap");
+			EntityManager em = emf.createEntityManager();
+			Map myMap = em.find(Map.class, new Map(1));
+			logger.debug(myMap);
+			
+		} catch (Exception ex) {
+			logger.error(ex);
+		}
+		return resultList;
+	}
 }
