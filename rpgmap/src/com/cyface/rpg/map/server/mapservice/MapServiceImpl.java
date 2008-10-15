0f903b7328dd31ee01f4c998b2838437ca48092d
cyface <cyface@1b8295e0-9937-11dd-bde8-492b52a95b86>
2008-10-15 11:18:10 +0000

    First shot at service implementations.  Not working = hibernate query mapping.

--- rpgmap/src/com/cyface/rpg/map/server/mapservice/MapServiceImpl.java
+++ rpgmap/src/com/cyface/rpg/map/server/mapservice/MapServiceImpl.java
@@ -1,14 +1,17 @@
 package com.cyface.rpg.map.server.mapservice;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import org.apache.log4j.Logger;
 
 import com.cyface.rpg.map.server.Map;
+import com.cyface.rpg.map.server.Point;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 public class MapServiceImpl extends RemoteServiceServlet {
@@ -22,9 +25,27 @@ public class MapServiceImpl extends RemoteServiceServlet {
 		try {
 			EntityManagerFactory emf = Persistence.createEntityManagerFactory("rpgmap");
 			EntityManager em = emf.createEntityManager();
-			Map myMap = em.find(Map.class, new Integer(1));
-			logger.debug(myMap);
-			resultList.add(myMap);
+			Query getAllMapsQuery = em.createQuery("select id, name, owner_id from map order by name");
+			List rawResultList = getAllMapsQuery.getResultList();
+			logger.debug(rawResultList);
+			resultList.addAll(rawResultList);
+			
+		} catch (Exception ex) {
+			logger.error(ex);
+		}
+		return resultList;
+	}
+	
+	ArrayList<Point> getAllPoints() {
+		ArrayList<Point> resultList = new ArrayList<Point>();
+		logger.debug("Aieee!");
+		try {
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("rpgmap");
+			EntityManager em = emf.createEntityManager();
+			Query getAllMapsQuery =  em.createQuery("select id, latitude, longitude, name from point order by name");
+			List rawResultList = getAllMapsQuery.getResultList();
+			logger.debug(rawResultList);
+			resultList.addAll(rawResultList);
 			
 		} catch (Exception ex) {
 			logger.error(ex);
