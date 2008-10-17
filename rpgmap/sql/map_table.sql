0a91125d712b2fac9711b8785b6dbe402c0fcd7f
cyface <cyface@1b8295e0-9937-11dd-bde8-492b52a95b86>
2008-10-17 02:54:14 +0000

    Added use/drop.

--- rpgmap/sql/map_table.sql
+++ rpgmap/sql/map_table.sql
@@ -1,4 +1,8 @@
-CREATE TABLE rpgmap.map (
+use rpgmap;
+
+DROP TABLE IF EXISTS map;
+
+CREATE TABLE map (
 	id  mediumint(8) unsigned NOT NULL auto_increment,
 	name varchar(2000) NOT NULL,
 	owner_id mediumint(8) unsigned NOT NULL,
