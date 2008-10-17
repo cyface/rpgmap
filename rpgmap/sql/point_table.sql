0a66e9fed6d6d8cdfb9b2365e4279ff97991fd16
cyface <cyface@1b8295e0-9937-11dd-bde8-492b52a95b86>
2008-10-17 02:53:46 +0000

    Added drop/use.

--- rpgmap/sql/point_table.sql
+++ rpgmap/sql/point_table.sql
@@ -1,20 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 2.9.1.1
--- http://www.phpmyadmin.net
--- 
--- Host: localhost
--- Generation Time: Oct 13, 2008 at 04:27 PM
--- Server version: 5.0.27
--- PHP Version: 5.2.0
--- 
--- Database: `rpgmap`
--- 
+USE rpgmap;
 
--- --------------------------------------------------------
-
--- 
--- Table structure for table `point`
--- 
+DROP TABLE IF EXISTS point;
 
 CREATE TABLE `point` (
   `id` mediumint(8) unsigned NOT NULL auto_increment,
