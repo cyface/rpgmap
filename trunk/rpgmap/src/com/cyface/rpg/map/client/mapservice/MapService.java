ac0af12a5308e95606a815ab8a487325f5072275
cyface <cyface@1b8295e0-9937-11dd-bde8-492b52a95b86>
2008-10-27 20:28:28 +0000

    First cut of working Hibernate end to end.

--- trunk/rpgmap/src/com/cyface/rpg/map/client/mapservice/MapService.java
+++ trunk/rpgmap/src/com/cyface/rpg/map/client/mapservice/MapService.java
@@ -2,8 +2,8 @@ package com.cyface.rpg.map.client.mapservice;
 
 import java.util.ArrayList;
 
-import com.cyface.rpg.map.client.entities.RPGMapMap;
-import com.cyface.rpg.map.client.entities.RPGMapPoint;
+import com.cyface.rpg.map.domain.entities.RPGMapMap;
+import com.cyface.rpg.map.domain.entities.RPGMapPoint;
 import com.google.gwt.user.client.rpc.RemoteService;
 
 public interface MapService extends RemoteService {
--- trunk/rpgmap/src/com/cyface/rpg/map/client/mapservice/MapServiceAsyncCallback.java
+++ trunk/rpgmap/src/com/cyface/rpg/map/client/mapservice/MapServiceAsyncCallback.java
@@ -1,21 +1,48 @@
 package com.cyface.rpg.map.client.mapservice;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
-import com.cyface.rpg.map.client.entities.RPGMapMap;
+import com.cyface.rpg.map.domain.entities.RPGMapMap;
+import com.cyface.rpg.map.domain.entities.RPGMapPoint;
+import com.google.gwt.maps.client.MapWidget;
+import com.google.gwt.maps.client.geom.LatLng;
+import com.google.gwt.maps.client.overlay.Marker;
+import com.google.gwt.maps.client.overlay.MarkerOptions;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public class MapServiceAsyncCallback implements AsyncCallback<ArrayList<RPGMapMap>> {
 
+	MapWidget parentMap;
+
+	public MapServiceAsyncCallback(MapWidget parentMap) {
+		this.parentMap = parentMap;
+	}
+
 	public void onFailure(Throwable error) {
 		Window.alert("Fail!");
-		
+
 	}
 
 	public void onSuccess(ArrayList<RPGMapMap> rpgMapList) {
-		Window.alert(rpgMapList.size() + "Size");
-		
-	}
+		if (this.parentMap != null && rpgMapList != null) {
+			Iterator<RPGMapMap> rpgMapListIterator = rpgMapList.iterator();
+			while (rpgMapListIterator.hasNext()) {
+				RPGMapMap currentMap = rpgMapListIterator.next();
+				if (currentMap.getChildRPGMapPoints() != null) {
+					Iterator<RPGMapPoint> childPointsIterator = currentMap.getChildRPGMapPoints().iterator();
+					while (childPointsIterator.hasNext()) {
+						RPGMapPoint currentPoint = childPointsIterator.next();
+						MarkerOptions newMarkerOptions = MarkerOptions.newInstance();
+						newMarkerOptions.setTitle(currentPoint.getName());
+						Marker newMarker = new Marker(LatLng.newInstance(currentPoint.getLatitude(), currentPoint.getLongitude()), newMarkerOptions);
+						newMarker.addMarkerClickHandler(new RPGMapMarkerClickHandler(parentMap, currentMap, currentPoint));
+						parentMap.addOverlay(newMarker);
+					}
+				}
+			}
+		}
 
+	}
 }
