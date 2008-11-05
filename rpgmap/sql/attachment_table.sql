77076598f47538ae8dcdd1218baa0e6fdd0cc18a
cyface <cyface@1b8295e0-9937-11dd-bde8-492b52a95b86>
2008-11-05 21:37:51 +0000

    Changed to more proper FK setup.

--- rpgmap/sql/attachment_table.sql
+++ rpgmap/sql/attachment_table.sql
@@ -5,9 +5,12 @@ DROP TABLE IF EXISTS attachment;
 CREATE TABLE `attachment` (
 	`id` MEDIUMINT( 8 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 	`name` VARCHAR( 100 ) NOT NULL ,
-	`parent_object_type` VARCHAR( 100 ) NOT NULL ,
-	`parent_object_id` MEDIUMINT( 8 ) NOT NULL ,
+	`parent_user_id` MEDIUMINT( 8 ),
+	`parent_map_id` MEDIUMINT( 8 ),
+	`parent_overlay_id` MEDIUMINT( 8 ),
 	`attachment` TEXT NOT NULL ,
 	`last_updated` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-	INDEX ( `parent_object_id` )
+	INDEX ( `parent_user_id` ),
+	INDEX ( `parent_map_id` ),
+	INDEX ( `parent_overlay_id` )
 ) ENGINE = MYISAM ;
