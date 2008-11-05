793f3438a005d8ac9551d4950fb8f1a0fb6accc0
cyface <cyface@1b8295e0-9937-11dd-bde8-492b52a95b86>
2008-11-05 17:29:33 +0000

    Moved WebContent to src/main/webapp.

--- rpgmap/sql/map_table.sql
+++ rpgmap/sql/map_table.sql
@@ -6,5 +6,6 @@ CREATE TABLE map (
 	id  mediumint(8) unsigned NOT NULL auto_increment,
 	name varchar(2000) NOT NULL,
 	owner_id mediumint(8) unsigned NOT NULL,
+	`last_updated` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   	PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
