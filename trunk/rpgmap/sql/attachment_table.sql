2fcd628092b0e84b160175fa9beb0fd70f1072fd
cyface <cyface@1b8295e0-9937-11dd-bde8-492b52a95b86>
2008-11-05 21:34:37 +0000

    Added object type and last updated.

--- trunk/rpgmap/sql/attachment_table.sql
+++ trunk/rpgmap/sql/attachment_table.sql
@@ -5,7 +5,9 @@ DROP TABLE IF EXISTS attachment;
 CREATE TABLE `attachment` (
 	`id` MEDIUMINT( 8 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 	`name` VARCHAR( 100 ) NOT NULL ,
+	`parent_object_type` VARCHAR( 100 ) NOT NULL ,
 	`parent_object_id` MEDIUMINT( 8 ) NOT NULL ,
 	`attachment` TEXT NOT NULL ,
+	`last_updated` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
 	INDEX ( `parent_object_id` )
 ) ENGINE = MYISAM ;
